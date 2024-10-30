d8b2e33fc2c2b70456dfd71d083e9559e6f3b54e
Jose M. Tobón <josem.tobon@udea.edu.co>
2024-10-29 20:00:40 -0500

    To transform fasta to nexus

--- sequences/gb.py
+++ sequences/gb.py
@@ -2,6 +2,7 @@
 Filter sequences of a genbank file based on gene type and specie
 """
 from Bio import SeqIO
+import pandas as pd
 
 
 with open('sequences.gb', 'r') as gb:
@@ -44,6 +45,19 @@ for record in records:
                     seen_species.add(specie)
                     break
 
+
+accession_df = pd.DataFrame(columns=["specie", "accession", "group"])
+for record in records_to_write:
+    specie = record.annotations["organism"].replace(" ", "_")
+    accession = record.id
+    if record.annotations["organism"].split()[0] == target_genera[-1]:
+        group = "outgroup"
+    else:
+        group = "ingroup"
+
+    accession_df.loc[len(accession_df)] = [specie, accession, group]
+
+
 species_count = {}
 for record in records_to_write:
     specie = record.annotations['organism']
@@ -55,3 +69,4 @@ for specie, count in species_count.items():
     print(f'{specie}: {count}')
 
 SeqIO.write(records_to_write, 'sequences_to_align.fasta', 'fasta')
+accession_df.to_csv("grupo_08_tabla_acceso.tsv", sep="\t")
