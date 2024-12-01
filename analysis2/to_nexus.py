70c799d32a1cc6b035a78ebf7a05291aa7044446
Jose M. Tobón <josem.tobon@udea.edu.co>
2024-12-01 17:41:05 -0500

    Old analysis

--- analysis2/to_nexus.py
+++
@@ -1,6 +0,0 @@
-from Bio import SeqIO
-
-input_file = "grupo_08_alineamiento_recortado.fasta"
-output_file = "grupo_08_alineamiento_recortado.nexus"
-
-SeqIO.convert(input_file, "fasta", output_file, "nexus", molecule_type="DNA")
